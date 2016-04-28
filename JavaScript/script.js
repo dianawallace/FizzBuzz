e27be5203ad083471417ab3008d89e24d97e4877
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-04-28 19:36:04 +0000

    refactoring done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,26 +1,24 @@
- $(document).ready(function() {
+jQuery(document).ready(function($) {
   
-  var input = prompt("Please input a number")
+    var input = prompt("Please input a number");
+    input = parseInt(input);
   
-  
-  var output = function() {
-  for (var i=1; i<101; i++ ) {
-    if( i%3 === 0 && i%5 === 0) {
-        $(".FizzBuzz").append("<li> FizzBuzz </li>");
-    }
-    else if (i%3 === 0) {
-        $(".FizzBuzz").append("<li> Fizz </li>");
-    } 
-    else if (i%5 === 0) {
-        $(".FizzBuzz").append("<li> Buzz </li>");
-    }
-    else {
-        output = i;
-        
-    }
-        console.log(output);
-    }
-}
+    var fizzBuzz = function(number) {  
+        var output;
+        for (var i=1; i<=number; i++ ) {
+            if( i%3 === 0 && i%5 === 0) {
+                output = '<li> FizzBuzz </li>';
+            } else if (i%3 === 0) {
+                output = '<li> Fizz </li>';
+            } else if (i%5 === 0) {
+                output = '<li> Buzz </li>';
+            } else {
+                output = '<li> ' + i + ' </li>';
+            }
+            $('.FizzBuzz').append(output);  
+        }
+    };
+    fizzBuzz(input);
 }); 
  
  
